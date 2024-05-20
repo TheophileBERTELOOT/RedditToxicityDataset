b56ead2e2ef1e8068955dcac527d9abcbb1bebb7
Loïc <baret.loic.1@gmail.com>
2024-05-20 17:05:11 -0400

    Some minor refactors

--- redditDataset/redditdataset/main.py
+++ redditDataset/redditdataset/main.py
@@ -1,19 +1,21 @@
-import praw
 import json
 
+import praw
 
-
-with open("../../../config.json", "r") as jsonfile:
+# with open("/home/lbaret/projects/RedditToxicityDataset/redditDataset/redditdataset/config.json", "r") as jsonfile:
+#     config = json.load(jsonfile)
+    
+with open("../../../config.json", "r") as jsonfile: # FIXME : brancher un fichier de config YAML à la place pour tout ce qui est chemin d'accès
     config = json.load(jsonfile)
 
 def cli():
     reddit = praw.Reddit(
-    client_id=config['client_id'],
-    client_secret=config['client_secret'],
-    password=config['password'],
-    user_agent="Comment Extraction (by u/USERNAME)",
-    username=config['username'],
-)
+        client_id=config['client_id'],
+        client_secret=config['client_secret'],
+        password=config['password'],
+        user_agent="Comment Extraction (by u/USERNAME)",
+        username=config['username'],
+    )
 
     subreddits = reddit.subreddits.popular()
     print(subreddits)
