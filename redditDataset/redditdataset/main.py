8eee83bbd8826a51f02f2ee452198c9f326d88ef
Loïc <baret.loic.1@gmail.com>
2024-05-25 21:55:08 -0400

    Directly fetch last comments

--- redditDataset/redditdataset/main.py
+++ redditDataset/redditdataset/main.py
@@ -2,28 +2,29 @@ import json
 
 import praw
 
-# with open("/home/lbaret/projects/RedditToxicityDataset/redditDataset/redditdataset/config.json", "r") as jsonfile:
-#     config = json.load(jsonfile)
-    
-with open("../../../config.json", "r") as jsonfile: # FIXME : brancher un fichier de config YAML à la place pour tout ce qui est chemin d'accès
+# FIXME : better with environment variables and not a json file.
+with open("/home/lbaret/projects/RedditToxicityDataset/redditDataset/redditdataset/config.json", "r") as jsonfile:
     config = json.load(jsonfile)
+    
+# with open("../../../config.json", "r") as jsonfile:
+#     config = json.load(jsonfile)
 
 def cli():
     reddit = praw.Reddit(
         client_id=config['client_id'],
         client_secret=config['client_secret'],
         password=config['password'],
-        user_agent="Comment Extraction (by u/USERNAME)",
+        user_agent="Comment Extraction (by u/Lobarten and u/DrMygalon)",
         username=config['username'],
     )
 
     subreddits = reddit.subreddits.popular()
-    print(subreddits)
-    for subereddit in subreddits:
-        for collection in subereddit.collections:
-            for submission in collection:
-                print(submission.title)
-                for comment in submission.comments:
-                    print(comment.author,comment.parent_id,comment.body)
+    for subreddit in subreddits:
+        print(f"From : {subreddit.title}")
+        for comment in subreddit.comments(limit=5):
+            print(comment.author)
+        
+        print(f"End for : {subreddit.title}\n")
+
 if __name__ == "__main__":
     cli()
